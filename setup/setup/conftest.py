bcbb453bc65374f747fe3928aa81c8805dd0d980
ErickMesquita <erick.mesquita@usp.br>
2023-02-05 21:24:52 -0300

    Initial Commit

---
+++ setup/setup/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from setup.users.models import User
+from setup.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
