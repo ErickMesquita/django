d96a494d0476c93e3c2d905029268f1f73a29b82
ErickMesquita <erick.mesquita@usp.br>
2023-02-11 20:51:10 -0300

    under local, accept all hosts

--- setup/settings.py
+++ setup/settings.py
@@ -46,7 +46,11 @@ SECRET_KEY = env(
     default="Dc1vuSkQVUXOFiqNUhFVyygQl1dXHGyPFPWDefDj1l0AqwPOs37vTak5rM2UYJnU",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "::1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1"]
+
+BUILD_ENV = env.str("BUILD_ENV", default=False)
+if BUILD_ENV == "local":
+	ALLOWED_HOSTS += ["*"]
 
 
 # Application definition
